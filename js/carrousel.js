529e63f142fd0f95e3acaf608c1d6b4050ab5c01
Luciano Oroquieta Merlino <oroquietaluciano@gmail.com>
2023-10-13 18:45:58 -0300

    refactor css

--- js/carrousel.js
+++ js/carrousel.js
@@ -3,26 +3,46 @@ document.addEventListener('DOMContentLoaded', function () {
     const flechaIzquierda = document.querySelector('.flecha-izquierda');
     const flechaDerecha = document.querySelector('.flecha-derecha');
     const cardWidth = document.querySelector('.card').offsetWidth; // Ancho de una tarjeta
-
+   
+    
     let currentIndex = 0; // Índice actual del carrusel
+
+
+
+    function updateNavigationButtons() {
+        flechaIzquierda.style.display = currentIndex === 0 ? 'none' : 'block';
+        flechaDerecha.style.display = currentIndex === carrusel.children.length - 1 ? 'none' : 'block';
+    }
 
     // Función para desplazar el carrusel hacia la izquierda
     function moveLeft() {
-        if (currentIndex > 0) {
+        
+
+        if(currentIndex > 0) {
             currentIndex--;
             carrusel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+           
+            updateNavigationButtons();
         }
     }
 
     // Función para desplazar el carrusel hacia la derecha
     function moveRight() {
+        
         if (currentIndex < carrusel.children.length - 1) {
             currentIndex++;
-            carrusel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;    
+            carrusel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+
+            updateNavigationButtons();
         }
     }
 
+    
+    
     // Event listeners para las flechas de navegación
     flechaIzquierda.addEventListener('click', moveLeft);
     flechaDerecha.addEventListener('click', moveRight);
+
+     // Actualiza los botones de navegación al cargar la página
+     updateNavigationButtons();
 });
