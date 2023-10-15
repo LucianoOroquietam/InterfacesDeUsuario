316db66d409837699950e90d3c92739c3da14c6e
Luciano Oroquieta Merlino <oroquietaluciano@gmail.com>
2023-10-15 20:34:16 -0300

    carrusel destacado funcionando

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,8 +1,16 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const carrusel = document.querySelector('.carrusel-destacado');
     const flechaIzquierda = document.querySelector('.flecha-izquierda');
     const flechaDerecha = document.querySelector('.flecha-derecha');
     const cardWidth = document.querySelector('.card').offsetWidth; // Ancho de una tarjeta
+    const card = document.querySelector('#card1');
+    const card2 = document.querySelector('#card2');
+    const card3 = document.querySelector('#card3');
+    const card4 = document.querySelector('#card4');
+    const card5 = document.querySelector('#card5');
+    const card6 = document.querySelector('#card6');
+    const card7 = document.querySelector('#card7');
    
     
     let currentIndex = 0; // Índice actual del carrusel
@@ -20,8 +28,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if(currentIndex > 0) {
             currentIndex--;
-            carrusel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-           
+            card.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card2.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card3.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card4.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card5.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card6.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card7.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
             updateNavigationButtons();
         }
     }
@@ -31,8 +44,13 @@ document.addEventListener('DOMContentLoaded', function () {
         
         if (currentIndex < carrusel.children.length - 1) {
             currentIndex++;
-            carrusel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-
+            card.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card2.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card3.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card4.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card5.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card6.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            card7.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
             updateNavigationButtons();
         }
     }
