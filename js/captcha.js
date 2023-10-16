5fdf3d1ab3341d024c44148cdacf3ba866219695
Luciano Oroquieta Merlino <oroquietaluciano@gmail.com>
2023-10-16 01:46:05 -0300

    animacion registrado

--- js/captcha.js
+++ js/captcha.js
@@ -4,14 +4,34 @@ document.addEventListener("DOMContentLoaded",loadPage);
 
 function loadPage(){
    const btnCaptcha = document.querySelector("#captcha");
+   const containerRegistrarse = document.querySelectorAll("#registro-exitoso");
+   const buttonActive = document.querySelectorAll("#button");
    
    btnCaptcha.addEventListener("click", checkedCaptcha);
+   
 
+   
    function checkedCaptcha(){
 
       btnCaptcha.classList.add("captcha-checkout");
       btnCaptcha.removeEventListener("click", checkedCaptcha);
-    
+
+
+      buttonActive.forEach((btn) => {
+         btn.classList.add("active");
+         btn.addEventListener("click", function () {
+             containerRegistrarse.forEach((contenedor) => {
+                 contenedor.classList.add("registrado");
+                 setTimeout(function () {
+                  window.location.href = 'index.html';
+              }, 2000);
+                
+             });
+         });
+     });
+   
    }
+
+   
 
 }
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,45 +1,53 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const flechaIzquierda = document.querySelector('.flecha-izquierda');
-    const flechaDerecha = document.querySelector('.flecha-derecha');
-    const cardWidth = document.querySelector('#card').offsetWidth; // Ancho de una tarjeta
-
+document.addEventListener("DOMContentLoaded", function () {
+    const flechaIzquierda = document.querySelector(".flecha-izquierda");
+    const flechaDerecha = document.querySelector(".flecha-derecha");
+    const cardWidth = document.querySelector("#card").offsetWidth; // Ancho de una tarjeta
+  
     const keyframeIzquierda = [
-        { transform: "perspective(250px) rotateY(20deg) translateZ(0)" },
-        { transform: "perspective(0px) rotateY(0deg) translateZ(0)" }
+        {transform: "skew(0deg, 0deg)"},
+        {transform: "skew(-10deg, 15deg)"},
+        {transform:"skew(0deg, 0deg)"}
     ];
     const keyframeDerecha = [
-        { transform: "perspective(250px) rotateY(-20deg) translateZ(0)" },
-        { transform: "perspective(0px) rotateY(0deg) translateZ(0)" }
+        {transform: "skew(0deg, 0deg)"},
+        {transform: "skew(10deg, -15deg)"},
+        {transform:"skew(0deg, 0deg)"}
     ];
-
-    const cards = document.querySelectorAll('#card');
+  
+    const cards = document.querySelectorAll("#card");
     const arrayCarrusel = Array.from(cards);
     let currentIndex = 0; // Índice actual del carrusel
-
+  
     function updateNavigationButtons() {
-        flechaIzquierda.style.display = currentIndex === 0 ? 'none' : 'block';
-        flechaDerecha.style.display = currentIndex === arrayCarrusel.length - 4 ? 'none' : 'block';
+      flechaIzquierda.style.display = currentIndex === 0 ? "none" : "block";
+      flechaDerecha.style.display =
+        currentIndex === arrayCarrusel.length - 4 ? "none" : "block";
     }
-
+  
     function moveCarrusel(direction) {
-        if (direction === 'left' && currentIndex > 0) {
-            currentIndex--;
-        } else if (direction === 'right' && currentIndex < arrayCarrusel.length - 4) {
-            currentIndex++;
-        }
-
-        arrayCarrusel.forEach((card, index) => {
-            card.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-           // const keyframe = direction === 'left' ? keyframeIzquierda : keyframeDerecha;
-           // card.animate(keyframe, 1000);
-        });
-
-        updateNavigationButtons();
+      if (direction === "left" && currentIndex > 0) {
+        currentIndex--;
+      } else if (
+        direction === "right" &&
+        currentIndex < arrayCarrusel.length - 4
+      ) {
+        currentIndex++;
+      }
+  
+      arrayCarrusel.forEach((card, index) => {
+        card.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+        const keyframe = direction === 'left' ? keyframeIzquierda : keyframeDerecha;
+        card.animate(keyframe, 2000);
+      });
+  
+      
+      updateNavigationButtons();
     }
-
-    flechaIzquierda.addEventListener('click', () => moveCarrusel('left'));
-    flechaDerecha.addEventListener('click', () => moveCarrusel('right'));
-
+  
+    flechaIzquierda.addEventListener("click", () => moveCarrusel("left"));
+    flechaDerecha.addEventListener("click", () => moveCarrusel("right"));
+  
     // Actualiza los botones de navegación al cargar la página
     updateNavigationButtons();
-});
+  });
+  
