43a668850082a8baaaf48cd65e111a680b6976e2
Luciano Oroquieta Merlino <oroquietaluciano@gmail.com>
2023-10-16 16:38:35 -0300

    animacion registro terminada

--- js/captcha.js
+++ js/captcha.js
@@ -6,6 +6,7 @@ function loadPage(){
    const btnCaptcha = document.querySelector("#captcha");
    const containerRegistrarse = document.querySelectorAll("#registro-exitoso");
    const buttonActive = document.querySelectorAll("#button");
+   const verificado = document.querySelector("#verificado");
    
    btnCaptcha.addEventListener("click", checkedCaptcha);
    
@@ -22,9 +23,10 @@ function loadPage(){
          btn.addEventListener("click", function () {
              containerRegistrarse.forEach((contenedor) => {
                  contenedor.classList.add("registrado");
+                 verificado.classList.add("show");
                  setTimeout(function () {
                   window.location.href = 'index.html';
-              }, 2000);
+              }, 3000);
                 
              });
          });
